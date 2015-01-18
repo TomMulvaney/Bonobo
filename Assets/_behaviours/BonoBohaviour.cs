ecaf2f1b81e779365ee5a6d7a9c1be70a1e0c0a7
TomMulvaney <mulvaney.tom@gmail.com>
2015-01-18 18:55:21 +0000

    Created Unity project. Created BonoBohaviour

---
+++ Assets/_behaviours/BonoBohaviour.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Bonobo
+{
+	// Pure Syntactic Sugar
+	public class BonoBohaviour : MonoBehaviour 
+	{
+		public virtual void On() {}
+		public virtual void Off() {}
+
+		public Vector3 pos
+		{
+			get
+			{
+				return transform.position;
+			}
+			set
+			{
+				transform.position = value;
+			}
+		}
+
+		public Vector3 localPos
+		{
+			get
+			{
+				return transform.localPosition;
+			}
+		}
+
+		public Vector3 euler
+		{
+			get
+			{
+				return transform.eulerAngles;
+			}
+			set
+			{
+				transform.eulerAngles = value;
+			}
+		}
+
+		public Vector3 localEuler
+		{
+			get
+			{
+				return transform.localEulerAngles;
+			}
+			set
+			{
+				transform.localEulerAngles = value;			
+			}
+		}
+
+		public Quaternion rot
+		{
+			get
+			{
+				return transform.rotation;
+			}
+		}
+
+		public Quaternion localRot
+		{
+			get
+			{
+				return transform.localRotation;
+			}
+		}
+
+
+		public Vector3 lossySca
+		{
+			get
+			{
+				return transform.lossyScale;
+			}
+		}
+
+		public Vector3 localSca
+		{
+			get
+			{
+				return transform.localScale;
+			}
+		}
+	}
+}
