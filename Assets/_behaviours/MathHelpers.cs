e16db20e3ea0c7fb9add9c0ef4854f28d01217e4
TomMulvaney <mulvaney.tom@gmail.com>
2015-01-18 19:12:54 +0000

    Updated TriggerTracker. Created CollectionHelpers and MathHelpers

---
+++ Assets/_behaviours/MathHelpers.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Bonobo
+{
+	public static class MathHelpers 
+	{
+		public static bool IsSameSign(float a, float b)
+		{
+			return Mathf.Approximately(a * b, Mathf.Abs(a) * Mathf.Abs(b));
+		}
+
+		public static bool IsSameSign(int a, int b)
+		{
+			return a * b == Mathf.Abs (a) * Mathf.Abs (b);
+		}
+	}
+}
--- Assets/_behaviours/TriggerTracker.cs
+++ Assets/_behaviours/TriggerTracker.cs
@@ -2,97 +2,100 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class TriggerTracker : MonoBehaviour 
+namespace Bonobo
 {
-    public delegate void TriggerTrackerEventHandler (TriggerTracker tracker, GameObject other);
-    public event TriggerTrackerEventHandler Entered;
-    public event TriggerTrackerEventHandler Exited;
-
-	List<GameObject> m_trackedObjects = new List<GameObject>();
-
-	// Removal and Defence
-    void Start()
-    {
-        StartCoroutine(RemoveNull());
-    }
-
-    IEnumerator RemoveNull()
-    {
-        yield return new WaitForSeconds(0.5f);
-        m_trackedObjects.RemoveAll(x => x == null);
-        StartCoroutine(RemoveNull());
-    }
-
-	void OnDisable()
+	public class TriggerTracker : MonoBehaviour 
 	{
-		m_trackedObjects.Clear();
-	}
+	    public delegate void TriggerTrackerEventHandler (TriggerTracker tracker, GameObject other);
+	    public event TriggerTrackerEventHandler Entered;
+	    public event TriggerTrackerEventHandler Exited;
 
-	// Enter
-	void OnTriggerEnter2D (Collider2D other) 
-	{
-		OnEnter (other.gameObject);
-	}
-	
-	void OnTriggerEnter(Collider other)
-	{
-		OnEnter (other.gameObject);
-	}
+		List<GameObject> m_trackedObjects = new List<GameObject>();
 
-	void OnEnter(GameObject other)
-	{
-		if (!m_trackedObjects.Contains(other))
+		// Removal and Defence
+	    void Start()
+	    {
+	        StartCoroutine(RemoveNull());
+	    }
+
+	    IEnumerator RemoveNull()
+	    {
+	        yield return new WaitForSeconds(0.5f);
+	        m_trackedObjects.RemoveAll(x => x == null);
+	        StartCoroutine(RemoveNull());
+	    }
+
+		void OnDisable()
 		{
-			m_trackedObjects.Add(other);
-			
-			if(Entered != null)
+			m_trackedObjects.Clear();
+		}
+
+		// Enter
+		void OnTriggerEnter2D (Collider2D other) 
+		{
+			OnEnter (other.gameObject);
+		}
+		
+		void OnTriggerEnter(Collider other)
+		{
+			OnEnter (other.gameObject);
+		}
+
+		void OnEnter(GameObject other)
+		{
+			if (!m_trackedObjects.Contains(other))
 			{
-				Entered(this, other);
+				m_trackedObjects.Add(other);
+				
+				if(Entered != null)
+				{
+					Entered(this, other);
+				}
 			}
 		}
-	}
 
-	// Exit
-	void OnTriggerExit2D (Collider2D other) 
-	{
-		OnExit (other.gameObject);
-	}
-
-	void OnTriggerExit(Collider other)
-	{
-		OnExit (other.gameObject);
-	}
-
-	void OnExit(GameObject other)
-	{
-		if(m_trackedObjects.Contains(other))
+		// Exit
+		void OnTriggerExit2D (Collider2D other) 
 		{
-			m_trackedObjects.Remove(other);
-			
-			if(Exited != null)
+			OnExit (other.gameObject);
+		}
+
+		void OnTriggerExit(Collider other)
+		{
+			OnExit (other.gameObject);
+		}
+
+		void OnExit(GameObject other)
+		{
+			if(m_trackedObjects.Contains(other))
 			{
-				Exited(this, other);
+				m_trackedObjects.Remove(other);
+				
+				if(Exited != null)
+				{
+					Exited(this, other);
+				}
 			}
 		}
-	}
 
-	// Getters
-	public bool IsTracking(GameObject go)
-	{
-		return m_trackedObjects.Contains(go);
-	}
-	
-	public int GetNumTrackedObjects()
-	{
-		return m_trackedObjects.Count;
-	}
+		// Getters
+		public bool IsTracking(GameObject go)
+		{
+			return m_trackedObjects.Contains(go);
+		}
+		
+		public int GetNumTrackedObjects()
+		{
+			return m_trackedObjects.Count;
+		}
 
-	// Logger
-    public void LogTrackedObjectNames()
-    {
-        foreach (GameObject go in m_trackedObjects)
-        {
-            Debug.Log(go.name);
-        }
-    }
+		// Logger
+	    public void LogTrackedObjectNames()
+	    {
+	        foreach (GameObject go in m_trackedObjects)
+	        {
+	            Debug.Log(go.name);
+	        }
+	    }
+	}
 }
