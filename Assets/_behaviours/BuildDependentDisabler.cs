4b8ce1fae291ea732a9707453c7d47393dab302a
TomMulvaney <mulvaney.tom@gmail.com>
2015-02-06 21:18:31 +0000

    Created BuildDependentDisabler and SpawningHelpers

---
+++ Assets/_behaviours/BuildDependentDisabler.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildDependentDisabler : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject[] m_gameObjects;
+	[SerializeField]
+	private MonoBehaviour[] m_behaviours;
+	[SerializeField]
+	private Platform[] m_disabledPlatforms;
+	[SerializeField]
+	private DebugSetting m_disabledDebugSetting;
+
+	enum Platform
+	{
+		Standalone,
+		PC,
+		Mac,
+		Linux,
+		iPhone,
+		Android,
+		Web,
+		Editor
+	}
+
+	enum DebugSetting
+	{
+		Neither,
+		Debug,
+		Production
+	}
+
+	void Awake () 
+	{
+		bool disable = (m_disabledDebugSetting == DebugSetting.Debug && Debug.isDebugBuild) || (m_disabledDebugSetting == DebugSetting.Production && !Debug.isDebugBuild);
+
+#if UNITY_STANDALONE
+		if(IsDisabled(Platform.Standalone))
+		{
+			disable = true;
+		}
+#elif UNITY_PC
+		if(IsDisabled(Platform.PC))
+		{
+			disable = true;
+		}
+#elif UNITY_MAC
+		if(IsDisabled(Platform.Mac))
+		{
+			disable = true;
+		}
+#elif UNITY_LINUX
+		if(IsDisabled(Platform.Linux))
+		{
+			disable = true;
+		}
+#elif UNITY_IPHONE
+		if(IsDisabled(Platform.iPhone))
+		{
+			disable = true;
+		}
+#elif UNITY_ANDROID
+		if(IsDisabled(Platform.Android))
+		{
+			disable = true;
+		}
+#elif UNITY_WEBPLAYER
+		if(IsDisabled(Platform.Web))
+		{
+			disable = true;
+		}
+#endif
+
+#if UNITY_EDITOR
+		if(IsDisabled(Platform.Editor))
+		{
+			disable = true;
+		}
+#endif
+
+		if (disable) 
+		{
+			foreach(GameObject go in m_gameObjects)
+			{
+				go.SetActive(false);
+			}
+
+			foreach(MonoBehaviour behaviour in m_behaviours)
+			{
+				behaviour.enabled = false;
+			}
+		}
+	}
+
+	bool IsDisabled(Platform platform)
+	{
+		return System.Array.IndexOf(m_disabledPlatforms, platform) != -1;
+	}
+}
---
+++ Assets/_behaviours/SpawningHelpers.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+
+namespace Bonobo
+{
+    public static class SpawningHelpers
+    {
+        public static GameObject SpawnPrefabTransforms(GameObject prefab, Transform transform)
+        {
+            GameObject newObject = (GameObject)GameObject.Instantiate(prefab);
+            newObject.transform.parent = transform;
+            newObject.transform.localRotation = prefab.transform.localRotation;
+            newObject.transform.localPosition = prefab.transform.localPosition;
+            newObject.transform.localScale = prefab.transform.localScale;
+
+            if (transform != null)
+            {
+                SetLayerRecursively(newObject, transform.gameObject.layer);
+            }
+
+            return newObject;
+        }
+
+        public static GameObject SpawnIdentityTransforms(GameObject prefab, Transform transform, bool scaleZero = false)
+        {
+            GameObject newObject = (GameObject)GameObject.Instantiate(prefab, transform.position, transform.rotation);
+            newObject.transform.parent = transform;
+            newObject.transform.localRotation = Quaternion.identity;
+            newObject.transform.localPosition = Vector3.zero;
+
+            Vector3 localScale = scaleZero ? Vector3.zero : Vector3.one;
+            newObject.transform.localScale = localScale;
+
+            if (transform != null)
+            {
+                SetLayerRecursively(newObject, transform.gameObject.layer);
+            }
+
+            return newObject;
+        }
+
+		static void SetLayerRecursively(GameObject newObject, int newLayer)
+		{
+			newObject.layer = newLayer;
+
+			foreach(Transform child in newObject.transform)
+			{
+				SetLayerRecursively(child.gameObject, newLayer);
+			}
+		}
+
+        public static Vector3 ReTranslateFromCamera(Vector3 inPos, Camera sourceCamera, Camera targetCamera, float depth)
+        {
+            Vector3 sourceScreen = sourceCamera.WorldToScreenPoint(inPos);
+
+            Ray sourceRay = targetCamera.ScreenPointToRay(sourceScreen);
+
+            Vector3 position = (sourceRay.origin + sourceRay.direction * depth);
+
+            return position;
+        }
+    }
+}
