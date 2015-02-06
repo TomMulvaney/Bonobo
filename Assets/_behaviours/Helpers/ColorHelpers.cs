4828841442c2c8e6d614b3f7b7e78b926b5653af
TomMulvaney <mulvaney.tom@gmail.com>
2015-02-06 21:35:00 +0000

    Created ColorHelpers and ColorTest. Renamed TestChamber_00 to NewTestChamber00

---
+++ Assets/_behaviours/Helpers/ColorHelpers.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Bonobo
+{
+	enum BonoboColor
+	{
+		Red,
+		Green,
+		Blue,
+		Yellow,
+		Pink,
+		Orange
+	}
+
+	public class ColorHelpers : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
---
+++ Assets/_behaviours/_proto/ColorTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
