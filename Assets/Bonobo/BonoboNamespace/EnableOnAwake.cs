9d77c46c3e4084a1103dbff70e73e25d655213f2
TomMulvaney <mulvaney.tom@gmail.com>
2015-02-07 11:25:30 +0000

    Created EnableOnAwake

---
+++ Assets/Bonobo/BonoboNamespace/EnableOnAwake.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Bonobo
+{
+    public class EnableOnAwake : MonoBehaviour 
+    {
+        [SerializeField]
+        private GameObject[] m_gameObjects;
+        [SerializeField]
+        private MonoBehaviour[] m_behaviours;
+
+        void Awake()
+        {
+            for (int i = 0; i < m_gameObjects.Length; ++i)
+            {
+                m_gameObjects[i].SetActive(true);
+            }
+
+            for(int i = 0; i < m_behaviours.Length; ++i)
+            {
+                m_behaviours[i].enabled = true;
+            }
+        }
+    }
+}
