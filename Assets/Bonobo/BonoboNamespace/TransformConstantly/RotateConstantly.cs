e28d7f26fb8d4c7f5e6ed9272084258cb45af500
TomMulvaney <mulvaney.tom@gmail.com>
2015-02-08 16:38:15 +0000

    Created ThrobConstantly

---
+++ Assets/Bonobo/BonoboNamespace/TransformConstantly/RotateConstantly.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Bonobo
+{
+    public class RotateConstantly : MonoBehaviour
+    {
+        [SerializeField]
+        private float m_rotateSpeed;
+        [SerializeField]
+        private float m_randomRange = 20;
+        [SerializeField]
+        private bool m_startRandom;
+        [SerializeField]
+        private bool m_clamp;
+
+        private float m_rotateSpeedInternal;
+
+        void Start()
+        {
+            if (m_startRandom)
+            {
+                transform.Rotate(Vector3.forward, Random.Range(0.0f, 360.0f));
+            }
+        }
+
+        void OnEnable()
+        {
+            m_rotateSpeedInternal = m_rotateSpeed + Random.Range(-m_randomRange, m_randomRange);
+        }
+
+    	void Update () 
+        {
+            transform.Rotate(Vector3.forward, m_rotateSpeedInternal * Time.deltaTime);
+    	}
+    }
+}
---
+++ Assets/Bonobo/BonoboNamespace/TransformConstantly/WobbleConstantly.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Bonobo
+{
+    public class WobbleConstantly : MonoBehaviour 
+    {
+        [SerializeField]
+        private float m_amount;
+        [SerializeField]
+        private float m_speed;
+        
+        private float m_current;
+        
+        void Start()
+        {
+            m_current = Random.Range(0.0f, 100.0f);
+        }
+        
+        // Update is called once per frame
+        void Update () 
+        {
+            m_current += Time.deltaTime * m_speed;
+            
+            transform.localPosition = new Vector3(
+                Mathf.Sin(m_current) + (Mathf.Cos(m_current * 1.42f) * 0.5f),
+                Mathf.Cos(m_current) + (Mathf.Sin(m_current * 0.87f) * 0.8f), 0) * m_amount;
+        }
+        
+        public void SetAmount(float myAmount)
+        {
+            m_amount = myAmount;
+        }
+        
+        public void SetSpeed(float mySpeed)
+        {
+            m_speed = mySpeed;
+        }
+    }
+}
