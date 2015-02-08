9ede3c2e4042a0c3c529b790df236718a9626420
TomMulvaney <mulvaney.tom@gmail.com>
2015-02-08 16:19:13 +0000

    Imported braid art assets and created BraidAtlas. Created ButtonSpriteSwap

---
+++ Assets/Bonobo/BonoboNamespace/NGUIDependent/ButtonSpriteSwap.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonSpriteSwap : MonoBehaviour 
+{
+    [SerializeField]
+    private UISprite m_sprite;
+    [SerializeField]
+    private string m_pressedName;
+
+    string m_unpressedName;
+
+	// Use this for initialization
+	void Awake () 
+    {
+        m_unpressedName = m_sprite.spriteName;
+	}
+	
+	void OnPress(bool pressed)
+    {
+        m_sprite.spriteName = pressed ? m_pressedName : m_unpressedName;
+    }
+}
